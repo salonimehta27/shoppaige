8c8452965dc5a658b1b5236d25dfb7439e73d82f
Saloni <salonimehta2127@gmail.com>
2022-01-21 02:29:06 -0500

    Added the first user instance and it's role defaults to a buyer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  after_create :assign_default_role
   rolify
   has_secure_password
+
+
+  private 
+    def assign_default_role
+      self.add_role(:buyer) if self.roles.blank?
+    end
+
 end
