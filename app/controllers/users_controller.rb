b2b4f93086076e2cf7877889d9822466d2edf34b
Saloni <salonimehta2127@gmail.com>
2022-01-21 03:02:14 -0500

    Added the code of users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+
+ def index 
+    user=User.all
+    render json:user
+ end
+
+ def show
+    user=User.find(params[:user_id])
+    render json: user, status: :ok
+ end
+
+ def create 
+    user=User.create!(user_params)
+    session[:user_id]=user.id
+    render json: user, status: :created
+ end
+
+ def destroy
+    user=User.find(params[:user_id])
+    user.delete
+    head :no_content
+ end
+
+ private 
+
+ def user_params
+    params.permit(:full_name,:email,:password,:password_confirmation)
+ end
+ 
 end
