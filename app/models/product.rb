b698fa3e3f2378af2a935631e22377794a6db75d
Saloni <salonimehta2127@gmail.com>
2022-01-22 19:38:58 -0500

    Added the category model and it's association with products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     resourcify
     belongs_to :user
+    has_one :category
 
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   get 'sessions/create'
   get 'sessions/destroy'
   resources :products
