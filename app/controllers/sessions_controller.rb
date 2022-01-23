4d457b68a0cb399a13442ba6aa52e8984a1c584e
Saloni <salonimehta2127@gmail.com>
2022-01-23 01:09:02 -0500

    Added few templates components for the features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,14 @@ class SessionsController < ApplicationController
     user=User.find_by(email:params[:email])
     if user&.authenticate(params[:password])
       session[:user_id]=user.id 
+      existing_cart=Cart.find_by(user_id:user.id)
+      if(existing_cart.user_id==user.id)
+        session[:cart_id]=existing_cart.id
+      else
+        cart=Cart.create!(total_items:0,total_amount:0)
+        cart.user_id=user.id 
+        session[:cart_id]=cart.id
+      end
       render json: user, status: :created
     else
       render json: {errors: ["Invalid email or password"]}, status: :unauthorized
